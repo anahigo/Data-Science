b78e60b3e9882c58f6317ede19c3cea560339efa
Ana Higo <55517497+anahigo@users.noreply.github.com>
2023-08-16 01:40:29 -0300

    Update Module12_Exercise_PartII.R

--- Module12_Exercise_PartII.R
+++ Module12_Exercise_PartII.R
@@ -1,45 +1,46 @@
-# Solução Lista de Exercícios - Capítulo 12
-
-# Obs: Caso tenha problemas com a acentuação, consulte este link:
-# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
-
-# Configurando o diretório de trabalho
-# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
-# Não use diretórios com espaço no nome
-setwd("C:/FCD/BigDataRAzure/Cap12")
 getwd()
 
-# Usaremos o dataset iris neste exemplo
-# O dataset iris possui observações de 3 especies de flores (Iris setosa, Iris virginica e Iris versocolor)
-# Para cada flor, 4 medidas sao usadas: 
-# comprimento (length) e largura (width) do caule (sepal) e comprimento e largura da petala (petal)
+# Part II
+# Packages
+install.packages("cluster")
 library(datasets)
-head(iris)
-
-# Análise exploratoria de dados com ggplot2
 library(ggplot2)
+library(cluster)
 
-# Veja que os dados claramente possui grupos com caracteristcas similares
+# We will use the iris dataset in this example
+# The iris dataset has observations of 3 species of flowers 
+#(Iris setosa, Iris virginica and Iris versocolor)
+# For each flower, 4 measurements are used:
+# length and width of the stem (sepal) and length and width of the petal (petal)
+
+View(iris)
+str(iris)
+round(prop.table(table(iris$Species)) * 100, digits = 1)
+
+# Exploratory data analysis with ggplot2
+# See that the data clearly has groups with similar characteristics
 ggplot(iris, aes(Petal.Length, Petal.Width, color = Species)) + geom_point(size = 3)
 
-# Agora usarmeos o K-Means para tentar agrupar os dados em clusters
+# Now we will use K-Means to try to group the data into clusters
 set.seed(101)
 help(kmeans)
 
-# Exercício 1 - Usando a função kmeans(), crie um modelo de clustering (aprendizagem não supervisionada). Use a documentação, para fazer sua pesquisa.
-# Neste caso, ja sabemos quantos grupos (clusters) existem em nossos dados (3)
-# Perceba que o dataset iris possui 5 colunas, mas estamos usando as 4 primeiras
+# Exercise 1 - Using the kmeans() function, create a clustering 
+# (unsupervised learning) model. Use the documentation to do your research.
+# In this case, we already know how many groups (clusters) exist in our data (3)
+# Notice that the iris dataset has 5 columns, but we are using the first 4
+irisCluster <- kmeans(iris[, 1:4], 3, nstart = 20)
+irisCluster
 
-
-# Obtendo informação sobre os clusters
-# Foram criados 3 clusters: cluster 1, 2 e 3
-# Perceba que apesar o algoritmo ter feito a divisão dos dados em clusters, houve problema em dividir alguns dos dados, 
-# que apesar de terem caracteristicas diferentes, ficaram no mesmo cluster
-
+# Getting information about the clusters
+# 3 clusters were created: cluster 1, 2 and 3
+# Note that even though the algorithm divided the data into clusters, 
+# there was a problem dividing some of the data,
+# that despite having different characteristics, were in the same cluster
+table(irisCluster$cluster, iris$Species)
+irisCluster
 
 # Visualizando os clusters
-install.packages("cluster")
-library(cluster)
 help(clusplot)
 
 # Plot
